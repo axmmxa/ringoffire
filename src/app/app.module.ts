6d82851f8f9d51b29c600201538b7bb705891098
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2022-11-30 11:21:06 -0300

    material design bug fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { StartScreenComponent } from './start-screen/start-screen.component';
 import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,9 +20,11 @@ import {MatIconModule} from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
-    MatIconModule
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
