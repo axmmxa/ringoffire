fa7708f1551cd8abf7803db66dedc8da5b09491d
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-01-04 12:12:20 +0100

    mobile-player created and first styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
+import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
 
 
 
@@ -32,6 +33,7 @@ import { AngularFireModule } from '@angular/fire/compat';
     PlayerComponent,
     DialogAddPlayerComponent,
     GameInfoComponent,
+    PlayerMobileComponent,
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
