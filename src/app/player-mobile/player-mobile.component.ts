8e67d2bb229ebadae20588c49e527e1e432ac419
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-01-07 11:31:16 +0100

    edit mobile player done

--- src/app/player-mobile/player-mobile.component.ts
+++ src/app/player-mobile/player-mobile.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PlayerMobileComponent implements OnInit {
 
   @Input() name;
-
+  @Input() image;
   @Input() playerActive:boolean = false;
 
 
